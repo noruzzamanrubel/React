f1acbbbca3cc01888c27092f3879c94d72f5dce9
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-05-06 17:15:29 +0600

    data pass from child to parent component

--- src/components/State_Lifting/Todo.js
+++ src/components/State_Lifting/Todo.js
@@ -2,11 +2,7 @@ import React from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const Todo = (props) => {
-  return (
-    <div>
-      <p key={uuidv4()}>{props.todo}</p>
-    </div>
-  );
+  return <p key={uuidv4()}>{props.todo}</p>;
 };
 
 export default Todo;

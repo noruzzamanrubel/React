75d7b6b0512dff8c7daa77bb9518ce381e5afa42
noruzzamanrubel <noruzzamanrubel@gmail.com>
2022-05-08 16:48:54 +0600

    data fetch and error handle in useEffect

--- src/components/UseEffect/DataFetch.js
+++ src/components/UseEffect/DataFetch.js
@@ -1,24 +1,44 @@
 import React, { useState, useEffect } from 'react'
 
+const ErrorMessage = "Data fatching not successfull. Please check your api url";
+
 const DataFetch = () => {
+
     const [todos, setTodos] = useState([]);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/todos/')
-            .then(response => response.json())
+        fetch('https://jsonplaceholder.typicode.com/todos/zd')
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error({ ErrorMessage });
+                } else {
+                    return response.json();
+                }
+            })
             .then((data) => {
                 setTodos(data);
             })
+            .catch((error) => {
+                setError(error.message);
+            }
+            );
     }, []);
+
+    const todolist = todos.map((todo) => {
+        return (
+            <p key={todo.id}>{todo.title}</p>
+        )
+    })
+
     return (
         <div className='text-center'>
-            <h1 className='heading'>DataFetch</h1>
-            {todos.map((todo) => {
-                return (
-                    <h4 key={todo.id}>{todo.title}</h4>
-                )
-            })}
+            <div>
+                <h1 className='heading'>DataFetch by UseEffect</h1>
+            </div>
+            {error && <h4>{ErrorMessage}</h4>}
+            {todolist}
         </div>
     )
 }
-a
 export default DataFetch
